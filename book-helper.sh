1981459643f2d2c27a071276e8b7e82ff23d836a
Manfred Morgner <manfred.morgner@gmx.net>
2012-04-24 14:05:46 +0300

    Update book-helper.sh

--- book-helper.sh
+++ book-helper.sh
@@ -1,30 +1,32 @@
 #! /usr/bin/env bash
 
-#echo Running $0
-
 LANG=$2
 
 IFS_OLD=$IFS
 IFS="
 "
+
 while read -r L
   do
+# schema "!de "
   if [[ "$L" =~ ^\![a-z][a-z]\  ]]
     then
+#   found language tag, $S becomes the sign
     S=`expr "$L" : '\!\(..\) .*'`
-#    echo LANGUAGE=+$S+
     if [ "$S" == "$LANG" ]
       then
+#     tag matches current mode, tag has to be removed from the line
       L=`expr "$L" : '\!.. \(.*\)'`
       else
+#     tag does not match current mode, line is to be ignored
       continue
       fi
     fi
 
+# depricated schema "%<german>"
   if [[ "$L" =~ ^\%\<.*\> ]]
     then
     S=`expr "$L" : '\%\<\(.*\)\>.*'`
-#    echo LANGUAGE=$S
     if [ "$S" == "$LANG" ]
       then
       L=`expr "$L" : '\%\<.*\>\(.*\)'`
@@ -35,13 +37,12 @@ while read -r L
 
   if [[ "$L" =~ \\input{.*} ]]
     then
-#    echo FOUND: $L
     F=`expr "$L" : '.*{\(.*\)}.*'`
-#    echo INPUT: $F
     $0 $F $2
     continue
     fi
 
   echo "$L"
   done < $1
+
 IFS=$IFS_OLD
