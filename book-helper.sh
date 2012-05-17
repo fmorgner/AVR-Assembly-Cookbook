ba49c1e9299e8fd18e057e05ab9dc3f7e4df1112
Manfred Morgner <manfred.morgner@gmx.net>
2012-05-17 20:01:11 +0200

    Look! It Runs with Linux!

--- book-helper.sh
+++ book-helper.sh
@@ -1,22 +1,47 @@
 #! /usr/bin/env bash
 
+#   FILE        : book-helper.sh
+#   COPYRIGHT   : (c) 2012 Manfred Mornger
+#   USED BY     : book-make.sh
+#
+#   TESTED WITH : MacOSX, Ubuntu
+#
+#   ====================================================================
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+FILE=$1
 LANG=$2
 
-IFS_OLD=$IFS
-IFS="
-"
+echo "% CALLED: $0 $1 $2"
 
-while read -r L
+IFS_OLD=$IFS
+IFS_NEW="
+"
+IFS=$IFTS_NEW
+
+while read -r LINE
   do
 # schema "!de "
-  if [[ "$L" =~ ^\![a-z][a-z]\  ]]
+  if [[ "$LINE" =~ ^\![a-z][a-z]\  ]]
     then
 #   found language tag, $S becomes the sign
-    S=`expr "$L" : '\!\(..\) .*'`
+    S=`expr "$LINE" : '\!\(..\) .*'`
     if [ "$S" == "$LANG" ]
       then
 #     tag matches current mode, tag has to be removed from the line
-      L=`expr "$L" : '\!.. \(.*\)'`
+      LINE=`expr "$LINE" : '\!.. \(.*\)'`
       else
 #     tag does not match current mode, line is to be ignored
       continue
@@ -24,25 +49,28 @@ while read -r L
     fi
 
 # depricated schema "%<german>"
-  if [[ "$L" =~ ^\%\<.*\> ]]
-    then
-    S=`expr "$L" : '\%\<\(.*\)\>.*'`
-    if [ "$S" == "$LANG" ]
-      then
-      L=`expr "$L" : '\%\<.*\>\(.*\)'`
-      else
-      continue
-      fi
-    fi
+#  if [[ "$LINE" =~ ^\%\<.*\> ]]
+#    then
+#    S=`expr "$LINE" : '\%\<\(.*\)\>.*'`
+#    if [ "$S" == "$LANG" ]
+#      then
+#      L=`expr "$LINE" : '\%\<.*\>\(.*\)'`
+#      else
+#      continue
+#      fi
+#    fi
 
-  if [[ "$L" =~ \\input{.*} ]]
+  if [[ "$LINE" =~ ^\\input\{.*\} ]]
     then
-    F=`expr "$L" : '.*{\(.*\)}.*'`
-    $0 $F $2
+    IFS=$IFS_OLD
+    INC=`expr "$LINE" : '.*{\(.*\)}.*'`
+    echo "% INCLUDED: $INC for LANG $2"
+    $0 $INC $2
+    IFS=$IFS_NEW
     continue
     fi
 
-  echo "$L"
-  done < $1
+  echo $LINE
+  done < $FILE
 
 IFS=$IFS_OLD
