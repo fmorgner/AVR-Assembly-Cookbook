ee3f9e7df23ee80cf21f0dac16dddcc450b2a0eb
Felix Morgner <felix.morgner@gmail.com>
2013-03-19 18:57:07 -0600

    fixed up the formatting of the code

--- Shift-Register/S000_shift-sram.asm
+++ Shift-Register/S000_shift-sram.asm
@@ -50,80 +50,79 @@
 ; At the end one fun fact: This program fits exactly into 100 bytes of program
 ; memory.
 
-.DEVICE atmega8p                       ; set controller type
+.DEVICE atmega8p ; set controller type
 
-.def    temp1 = r16                     ; Register for temporary storage
-.def    clock = r17                     ; Register for the shift-register clock selection
-.def	zero  = r18			; Register representing a 0
+.def    temp1 = r16 ; Register for temporary storage
+.def    clock = r17 ; Register for the shift-register clock selection
+.def    zero  = r18 ; Register representing a 0
 
-.org    0x0000
-        rjmp    setup                   ; 'setup' als Start-Routine registrieren
+.org    0
 
 setup:
-        ldi     temp1,  HIGH(RAMEND)    ; Stackpointer initialization
-        out     SPH,    temp1           ; -- " --
-        ldi     temp1,  LOW (RAMEND)    ; -- " --
-        out     SPL,    temp1           ; -- " --
+    ldi     temp1,  HIGH(RAMEND)    ; Stackpointer initialization
+    out     SPH,    temp1           ; -- " --
+    ldi     temp1,  LOW (RAMEND)    ; -- " --
+    out     SPL,    temp1           ; -- " --
 
-	clr	zero
-        
-        ldi     temp1,  $01             ; Set PORTD0 to output
-        out     DDRD,   temp1           ; -- " --
+    clr     zero
 
-        ldi     temp1,  $03             ; Set PORTC0 and PORTC1 to output
-        out     DDRC,   temp1           ; -- " --
-        
-	ldi	temp1,	$aa		; Fill the "leds" byte with 10101010
-	sts	leds,	temp1
+    ldi     temp1,  $01             ; Set PORTD0 to output
+    out     DDRD,   temp1           ; -- " --
 
-	ldi	clock,	$01		; Select data clock mode                       
+    ldi     temp1,  $03             ; Set PORTC0 and PORTC1 to output
+    out     DDRC,   temp1           ; -- " --
+
+    ldi     temp1,  $aa             ; Fill the "leds" byte with 10101010
+    sts     leds,   temp1           ; -- " --
+
+    ldi     clock,  $01             ; Select data clock mode                       
 
 main:
-	lds	temp1,	leds		; Get the LED data from SRAM into the temporary register
-	
-	out	PORTD,	temp1		; Output the state of the first LED to PORTD
-	out	PORTC,	clock		; Clock the first bit into the storage register
-	out	PORTC,	zero		; -- " --
+    lds     temp1,  leds            ; Get the LED data from SRAM into the temporary register
 
-	lsr	temp1			; Shift temp1 right so we can output the second bit
-	out	PORTD,	temp1		; Output the state of the second LED to PORTD
-	out	PORTC,	clock		; Clock the second bit into the storage register
-	out	PORTC,	zero		; -- " --
+    out     PORTD,  temp1           ; Output the state of the first LED to PORTD
+    out     PORTC,  clock           ; Clock the first bit into the storage register
+    out     PORTC,  zero            ; -- " --
 
-	lsr	temp1			; Shift temp1 right so we can output the third bit
-	out	PORTD,	temp1		; Output the state of the third LED to PORTD
-	out	PORTC,	clock		; Clock the third bit into the storage register
-	out	PORTC,	zero		; -- " --
+    lsr     temp1                   ; Shift temp1 right so we can output the second bit
+    out     PORTD,  temp1           ; Output the state of the second LED to PORTD
+    out     PORTC,  clock           ; Clock the second bit into the storage register
+    out     PORTC,  zero            ; -- " --
 
-	lsr	temp1			; Shift temp1 right so we can output the fourth bit
-	out	PORTD,	temp1		; Output the state of the fourth LED to PORTD
-	out	PORTC,	clock		; Clock the fourth bit into the storage register
-	out	PORTC,	zero		; -- " --
+    lsr     temp1                   ; Shift temp1 right so we can output the third bit
+    out     PORTD,  temp1           ; Output the state of the third LED to PORTD
+    out     PORTC,  clock           ; Clock the third bit into the storage register
+    out     PORTC,  zero            ; -- " --
 
-	lsr	temp1			; Shift temp1 right so we can output the fifth bit
-	out	PORTD,	temp1		; Output the state of the fifth LED to PORTD
-	out	PORTC,	clock		; Clock the fifth bit into the storage register
-	out	PORTC,	zero		; -- " --
+    lsr     temp1                   ; Shift temp1 right so we can output the fourth bit
+    out     PORTD,  temp1           ; Output the state of the fourth LED to PORTD
+    out     PORTC,  clock           ; Clock the fourth bit into the storage register
+    out     PORTC,  zero            ; -- " --
 
-	lsr	temp1			; Shift temp1 right so we can output the sixth bit
-	out	PORTD,	temp1		; Output the state of the sixth LED to PORTD
-	out	PORTC,	clock		; Clock the sixth bit into the storage register
-	out	PORTC,	zero		; -- " --
+    lsr     temp1                   ; Shift temp1 right so we can output the fifth bit
+    out     PORTD,  temp1           ; Output the state of the fifth LED to PORTD
+    out     PORTC,  clock           ; Clock the fifth bit into the storage register
+    out     PORTC,  zero            ; -- " --
 
-	lsr	temp1			; Shift temp1 right so we can output the seventh bit
-	out	PORTD,	temp1		; Output the state of the seventh LED to PORTD
-	out	PORTC,	clock		; Clock the seventh bit into the storage register
-	out	PORTC,	zero		; -- " --
+    lsr     temp1                   ; Shift temp1 right so we can output the sixth bit
+    out     PORTD,  temp1           ; Output the state of the sixth LED to PORTD
+    out     PORTC,  clock           ; Clock the sixth bit into the storage register
+    out     PORTC,  zero            ; -- " --
 
-	lsr	temp1			; Shift temp1 right so we can output the eighth bit
-	out	PORTD,	temp1		; Output the state of the eighth LED to PORTD
-	out	PORTC,	clock		; Clock the eighth bit into the storage register
-	out	PORTC,	zero		; -- " --
+    lsr     temp1                   ; Shift temp1 right so we can output the seventh bit
+    out     PORTD,  temp1           ; Output the state of the seventh LED to PORTD
+    out     PORTC,  clock           ; Clock the seventh bit into the storage register
+    out     PORTC,  zero            ; -- " --
 
-	lsl	clock			; Select latch clock mode
-	out	PORTC,	clock		; Latch data into the output register
-	out	PORTC,	zero		; -- " --
+    lsr     temp1                   ; Shift temp1 right so we can output the eighth bit
+    out     PORTD,  temp1           ; Output the state of the eighth LED to PORTD
+    out     PORTC,  clock           ; Clock the eighth bit into the storage register
+    out     PORTC,  zero            ; -- " --
+
+    lsl     clock                   ; Select latch clock mode
+    out     PORTC,  clock           ; Latch data into the output register
+    out     PORTC,  zero            ; -- " --
 
 .dseg
 
-leds:	.byte	1			; Reserve one byte in SRAM for the storage of the LED status
+leds:   .byte   1 ; Reserve one byte in SRAM for the storage of the LED status
